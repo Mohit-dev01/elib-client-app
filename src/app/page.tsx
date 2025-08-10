53f5de46ccc409adc9fb9905f738fa38079d0384
Mohit Wadhwa <mohitwadhwa7788@gmail.com>
2025-08-10 14:22:12 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
 
 export default function Home() {
-  return <div>Welocme to elib.</div>;
+  return (
+    <>
+      <Banner />
+    </>
+  );
 }
